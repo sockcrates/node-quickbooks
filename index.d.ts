f6bb382a00591414343b6d03ea84e023d406441c
sockcrates <17348049+sockcrates@users.noreply.github.com>
2021-01-25 12:56:02 -0500

    Implement Record

--- index.d.ts
+++ index.d.ts
@@ -124,172 +124,172 @@ declare class node_quickbooks {
 
   findAccounts(
     criteria: string | Array<APIFindQuery> | undefined,
-    callback: { (err: any, res: APIFindQueryResponse<any>): void },
+    callback: { (err: any, res: APIFindQueryResponse<string, any>): void },
   ): void;
 
   findAttachables(
     criteria: string | Array<APIFindQuery> | undefined,
-    callback: { (err: any, res: APIFindQueryResponse<any>): void },
+    callback: { (err: any, res: APIFindQueryResponse<string, any>): void },
   ): void;
 
   findBillPayments(
     criteria: string | Array<APIFindQuery> | undefined,
-    callback: { (err: any, res: APIFindQueryResponse<any>): void },
+    callback: { (err: any, res: APIFindQueryResponse<string, any>): void },
   ): void;
 
   findBills(
     criteria: string | Array<APIFindQuery> | undefined,
-    callback: { (err: any, res: APIFindQueryResponse<any>): void },
+    callback: { (err: any, res: APIFindQueryResponse<string, any>): void },
   ): void;
 
   findBudgets(
     criteria: string | Array<APIFindQuery> | undefined,
-    callback: { (err: any, res: APIFindQueryResponse<any>): void },
+    callback: { (err: any, res: APIFindQueryResponse<string, any>): void },
   ): void;
 
   findClasses(
     criteria: string | Array<APIFindQuery> | undefined,
-    callback: { (err: any, res: APIFindQueryResponse<any>): void },
+    callback: { (err: any, res: APIFindQueryResponse<string, any>): void },
   ): void;
 
   findCompanyInfos(
     criteria: string | Array<APIFindQuery> | undefined,
-    callback: { (err: any, res: APIFindQueryResponse<any>): void },
+    callback: { (err: any, res: APIFindQueryResponse<string, any>): void },
   ): void;
 
   findCreditMemos(
     criteria: string | Array<APIFindQuery> | undefined,
-    callback: { (err: any, res: APIFindQueryResponse<any>): void },
+    callback: { (err: any, res: APIFindQueryResponse<string, any>): void },
   ): void;
 
   findCustomerTypes(
     criteria: string | Array<APIFindQuery> | undefined,
-    callback: { (err: any, res: APIFindQueryResponse<any>): void },
+    callback: { (err: any, res: APIFindQueryResponse<string, any>): void },
   ): void;
 
   findCustomers(
     criteria: string | Array<APIFindQuery> | undefined,
-    callback: { (err: any, res: APIFindQueryResponse<Customer>): void },
+    callback: { (err: any, res: APIFindQueryResponse<string, Customer>): void },
   ): void;
 
   findDepartments(
     criteria: string | Array<APIFindQuery> | undefined,
-    callback: { (err: any, res: APIFindQueryResponse<any>): void },
+    callback: { (err: any, res: APIFindQueryResponse<string, any>): void },
   ): void;
 
   findDeposits(
     criteria: string | Array<APIFindQuery> | undefined,
-    callback: { (err: any, res: APIFindQueryResponse<any>): void },
+    callback: { (err: any, res: APIFindQueryResponse<string, any>): void },
   ): void;
 
   findEmployees(
     criteria: string | Array<APIFindQuery> | undefined,
-    callback: { (err: any, res: APIFindQueryResponse<any>): void },
+    callback: { (err: any, res: APIFindQueryResponse<string, any>): void },
   ): void;
 
   findEstimates(
     criteria: string | Array<APIFindQuery> | undefined,
-    callback: { (err: any, res: APIFindQueryResponse<any>): void },
+    callback: { (err: any, res: APIFindQueryResponse<string, any>): void },
   ): void;
 
   findExchangeRates(
     criteria: string | Array<APIFindQuery> | undefined,
-    callback: { (err: any, res: APIFindQueryResponse<any>): void },
+    callback: { (err: any, res: APIFindQueryResponse<string, any>): void },
   ): void;
 
   findInvoices(
     criteria: string | Array<APIFindQuery> | undefined,
-    callback: { (err: any, res: APIFindQueryResponse<any>): void },
+    callback: { (err: any, res: APIFindQueryResponse<string, any>): void },
   ): void;
 
   findItems(
     criteria: string | Array<APIFindQuery> | undefined,
-    callback: { (err: any, res: APIFindQueryResponse<Item>): void },
+    callback: { (err: any, res: APIFindQueryResponse<string, Item>): void },
   ): void;
 
   findJournalCodes(
     criteria: string | Array<APIFindQuery> | undefined,
-    callback: { (err: any, res: APIFindQueryResponse<any>): void },
+    callback: { (err: any, res: APIFindQueryResponse<string, any>): void },
   ): void;
 
   findJournalEntries(
     criteria: string | Array<APIFindQuery> | undefined,
-    callback: { (err: any, res: APIFindQueryResponse<any>): void },
+    callback: { (err: any, res: APIFindQueryResponse<string, any>): void },
   ): void;
 
   findPaymentMethods(
     criteria: string | Array<APIFindQuery> | undefined,
-    callback: { (err: any, res: APIFindQueryResponse<any>): void },
+    callback: { (err: any, res: APIFindQueryResponse<string, any>): void },
   ): void;
 
   findPayments(
     criteria: string | Array<APIFindQuery> | undefined,
-    callback: { (err: any, res: APIFindQueryResponse<any>): void },
+    callback: { (err: any, res: APIFindQueryResponse<string, any>): void },
   ): void;
 
   findPreferenceses(
     criteria: string | Array<APIFindQuery> | undefined,
-    callback: { (err: any, res: APIFindQueryResponse<any>): void },
+    callback: { (err: any, res: APIFindQueryResponse<string, any>): void },
   ): void;
 
   findPurchaseOrders(
     criteria: string | Array<APIFindQuery> | undefined,
-    callback: { (err: any, res: APIFindQueryResponse<any>): void },
+    callback: { (err: any, res: APIFindQueryResponse<string, any>): void },
   ): void;
 
   findPurchases(
     criteria: string | Array<APIFindQuery> | undefined,
-    callback: { (err: any, res: APIFindQueryResponse<any>): void },
+    callback: { (err: any, res: APIFindQueryResponse<string, any>): void },
   ): void;
 
   findRefundReceipts(
     criteria: string | Array<APIFindQuery> | undefined,
-    callback: { (err: any, res: APIFindQueryResponse<any>): void },
+    callback: { (err: any, res: APIFindQueryResponse<string, any>): void },
   ): void;
 
   findSalesReceipts(
     criteria: string | Array<APIFindQuery> | undefined,
-    callback: { (err: any, res: APIFindQueryResponse<any>): void },
+    callback: { (err: any, res: APIFindQueryResponse<string, any>): void },
   ): void;
 
   findTaxAgencies(
     criteria: string | Array<APIFindQuery> | undefined,
-    callback: { (err: any, res: APIFindQueryResponse<any>): void },
+    callback: { (err: any, res: APIFindQueryResponse<string, any>): void },
   ): void;
 
   findTaxCodes(
     criteria: string | Array<APIFindQuery> | undefined,
-    callback: { (err: any, res: APIFindQueryResponse<any>): void },
+    callback: { (err: any, res: APIFindQueryResponse<string, any>): void },
   ): void;
 
   findTaxRates(
     criteria: string | Array<APIFindQuery> | undefined,
-    callback: { (err: any, res: APIFindQueryResponse<any>): void },
+    callback: { (err: any, res: APIFindQueryResponse<string, any>): void },
   ): void;
 
   findTerms(
     criteria: string | Array<APIFindQuery> | undefined,
-    callback: { (err: any, res: APIFindQueryResponse<any>): void },
+    callback: { (err: any, res: APIFindQueryResponse<string, any>): void },
   ): void;
 
   findTimeActivities(
     criteria: string | Array<APIFindQuery> | undefined,
-    callback: { (err: any, res: APIFindQueryResponse<any>): void },
+    callback: { (err: any, res: APIFindQueryResponse<string, any>): void },
   ): void;
 
   findTransfers(
     criteria: string | Array<APIFindQuery> | undefined,
-    callback: { (err: any, res: APIFindQueryResponse<any>): void },
+    callback: { (err: any, res: APIFindQueryResponse<string, any>): void },
   ): void;
 
   findVendorCredits(
     criteria: string | Array<APIFindQuery> | undefined,
-    callback: { (err: any, res: APIFindQueryResponse<any>): void },
+    callback: { (err: any, res: APIFindQueryResponse<string, any>): void },
   ): void;
 
   findVendors(
     criteria: string | Array<APIFindQuery> | undefined,
-    callback: { (err: any, res: APIFindQueryResponse<any>): void },
+    callback: { (err: any, res: APIFindQueryResponse<string, any>): void },
   ): void;
 
   getAccount(id: any, callback: any): void;
@@ -553,10 +553,11 @@ export interface APIFindQuery {
   operator: string;
 }
 
-export interface APIFindQueryResponse<T> {
-  QueryResponse: {
-    [key: string]: Array<T>;
-  }
+export interface APIFindQueryResponse<K extends string, T> {
+  QueryResponse: Record<K, Array<T>> & {
+    maxResults: number;
+    startPosition: number;
+  };
   time: string;
 }
 
